676f9766eae7fd538a328fe3195b1b19dd7e29e2
Bryan <bryanmuliawan@yahoo.co.id>
2022-01-06 14:01:34 +0700

    Health potion

--- Assets/Script/ItemUse.cs
+++ Assets/Script/ItemUse.cs
@@ -5,13 +5,18 @@ using UnityEngine.UI;
 
 public class ItemUse : MonoBehaviour
 {
+    public string itemName;
     public PlayerInventory playerInventory;
     public int inventoryIndex;
     public GameObject itemAmountText;
+    public GameObject[] items;
+
+    private int healthPotion;
 
     // Start is called before the first frame update
     void Start()
     {
+        healthPotion = 5;
         playerInventory = GameObject.FindGameObjectWithTag("player").GetComponent<PlayerInventory>();
     }
 
@@ -23,14 +28,32 @@ public class ItemUse : MonoBehaviour
 
     public void Use()
     {
+        // Remove 1 item from inventory
         playerInventory.itemAmount[inventoryIndex]--;
         itemAmountText.GetComponent<Text>().text = playerInventory.itemAmount[inventoryIndex].ToString();
 
+        // Use item
+        UseItem();
+
+        // Remove item if run out of item
         if (playerInventory.itemAmount[inventoryIndex] == 0)
         {
             Destroy(gameObject);
             Destroy(itemAmountText);
             playerInventory.isFull[inventoryIndex] = false;
+        }
+    }
+
+    void UseItem()
+    {
+        switch (itemName)
+        {
+            case "health_potion":
+                PlayerStats.playerStats.HealCharacter(healthPotion);
+                break;
+            default:
+                // code block
+                break;
         }
     }
 }
