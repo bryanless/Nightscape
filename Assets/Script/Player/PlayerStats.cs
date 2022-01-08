04ca872b7a4f2f871ace2f5781d1ac7222433410
Daniel Aprillio <danielaprillio02@gmail.com>
2022-01-08 21:26:09 +0700

    shield regeneration

--- Assets/Script/Player/PlayerStats.cs
+++ Assets/Script/Player/PlayerStats.cs
@@ -11,9 +11,14 @@ public class PlayerStats : MonoBehaviour
     public GameObject player;
     public Text healthText;
     public Slider healthSlider;
+    public Text shieldText;
+    public Slider shieldSlider;
 
     public float health;
     public float maxHealth;
+
+    public float shield;
+    public float maxShield;
 
     public int coins_gold, coins_silver, coins_bronze;
     public Text goldCoinsValueText, silverCoinsValueText, bronzeCoinsValueText;
@@ -37,7 +42,10 @@ public class PlayerStats : MonoBehaviour
     void Start()
     {
         health = maxHealth;
+        maxShield = Mathf.Ceil(maxHealth / 3);
+        shield = maxShield;
         setHealthUI();
+        setShieldUI();
         setCoinsUI();
     }
 
@@ -50,9 +58,20 @@ public class PlayerStats : MonoBehaviour
 
     public void DealDamage(float damage)
     {
-        health -= damage;
+        float shieldDamage = (damage / 4);
+        shield -= shieldDamage;
+
+        StartCoroutine(RegenShield());
+
+        if (shield <= 0)
+        {
+            shield = 0;
+            health -= damage;
+        }
+
         CheckDeath();
         setHealthUI();
+        setShieldUI();
     }
 
     public void HealCharacter(float heal)
@@ -62,10 +81,30 @@ public class PlayerStats : MonoBehaviour
         setHealthUI();
     }
 
+    private IEnumerator RegenShield()
+    {
+        yield return new WaitForSeconds(5);
+
+        while (shield < maxShield)
+        {
+            shield += 1;
+            CheckOverHeal();
+            setShieldUI();
+            yield return new WaitForSeconds(5);
+        }
+
+    }
+
     private void setHealthUI()
     {
         healthSlider.value = CalculateHealthPercentage();
         healthText.text = Mathf.Ceil(health).ToString() + " / " + Mathf.Ceil(maxHealth).ToString();
+    }
+
+    private void setShieldUI()
+    {
+        shieldSlider.value = CalculateShieldPercentage();
+        shieldText.text = Mathf.Ceil(shield).ToString() + " / " + Mathf.Ceil(maxShield).ToString();
     }
 
     private void CheckOverHeal()
@@ -73,6 +112,10 @@ public class PlayerStats : MonoBehaviour
         if (health > maxHealth)
         {
             health = maxHealth;
+        }
+        if (shield > maxShield)
+        {
+            shield = maxShield;
         }
     }
 
@@ -87,6 +130,11 @@ public class PlayerStats : MonoBehaviour
     float CalculateHealthPercentage()
     {
         return (health / maxHealth);
+    }
+
+    float CalculateShieldPercentage()
+    {
+        return (shield / maxShield);
     }
 
     public void AddCoins(CoinsPickup coins)
